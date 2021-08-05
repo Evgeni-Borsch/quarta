90e600da1d18c9b3bc1cbd6a3212e4def03777f9
Feodor Rusanov <f.rusanov@gmail.com>
2021-08-05 03:09:18 +0300

    🍱 Add font

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'Header',
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped></style>
